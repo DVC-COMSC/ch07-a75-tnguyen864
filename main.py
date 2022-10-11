eaa2fe3bc9b453c76a45faeee7329275e7f66365
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 21:25:34 -0700

    Assignment 7-5

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 import sys
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
 if len(num1) > len(num2):
-	print ('False')
+	print ("False")
 	sys.exit(0)
-# ******************************
-# Make your Code
-# ******************************
 
-# print ('True') or ('False')
+isSubset = ', '.join(map(str, num1)) in ', '.join(map(str, num2))
+if isSubset:
+	print("True")
+else:
+	print("False")
